6865c915921764983c74860d9d955790241668ac
Sadra <sadradarvishi444@gmail.com>
2025-08-21 13:20:23 +0330

    changed setting and .env.example

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h9qyfztff&#iw71csa_ng8pz7hxgw8z%43v*$!kr-_#a4cx!bt'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
